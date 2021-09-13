591b19332fd28e92eb7b1c58e64e85fb02305952
Gerog13 <gerog13@hotmail.com>
2021-09-13 20:18:38 -0300

    Maps-advisor-app v1

---
+++ src/api/index.js
@@ -0,0 +1,59 @@
+import axios from "axios";
+
+const options = {
+  params: {
+    bl_latitude: "11.847676",
+    tr_latitude: "12.838442",
+    bl_longitude: "109.095887",
+    tr_longitude: "109.149359",
+  },
+  headers: {
+    "x-rapidapi-host": "travel-advisor.p.rapidapi.com",
+    "x-rapidapi-key": "f3d75e7de8msh4e9ef16886f8ff0p108f0ejsna537a75e989e",
+  },
+};
+
+export const getPlacesData = async (type, sw, ne) => {
+  try {
+    const {
+      data: { data },
+    } = await axios.get(
+      `https://travel-advisor.p.rapidapi.com/${type}/list-in-boundary`,
+      {
+        params: {
+          bl_latitude: sw.lat,
+          tr_latitude: ne.lat,
+          bl_longitude: sw.lng,
+          tr_longitude: ne.lng,
+        },
+        headers: {
+          "x-rapidapi-host": "travel-advisor.p.rapidapi.com",
+          "x-rapidapi-key":
+            process.env.REACT_APP_RAPIDAPI_KEY,
+        },
+      }
+    );
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const getWeatherData = async (lat, lng) => {
+  try {
+    const { data } = await axios.get(
+      "https://community-open-weather-map.p.rapidapi.com/find",
+      {
+        params: { lat, lon: lng },
+        headers: {
+          "x-rapidapi-host": "community-open-weather-map.p.rapidapi.com",
+          "x-rapidapi-key":
+          process.env.REACT_APP_RAPIDAPI_KEY,
+        },
+      }
+    );
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
